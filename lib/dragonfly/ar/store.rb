cf85c825f136c1a6b51c5b79a15f61e0a67cab19
Julien Letessier <julien.letessier@gmail.com>
2014-01-02 10:07:26 +0000

    Gem scaffolding.

---
+++ lib/dragonfly/ar/store.rb
@@ -0,0 +1,9 @@
+require "dragonfly/ar/store/version"
+
+module Dragonfly
+  module Ar
+    module Store
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/dragonfly/ar/store/version.rb
@@ -0,0 +1,7 @@
+module Dragonfly
+  module Ar
+    module Store
+      VERSION = "0.0.1"
+    end
+  end
+end
