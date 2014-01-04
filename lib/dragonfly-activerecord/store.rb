41b2981dc73b644a077094c3aeb6b98731a831af
Julien Letessier <julien.letessier@gmail.com>
2014-01-04 17:33:29 +0000

    Avoids spurious writes

--- lib/dragonfly-activerecord/store.rb
+++ lib/dragonfly-activerecord/store.rb
@@ -7,20 +7,20 @@ module Dragonfly::ActiveRecord
 
     # +temp_object+ should respond to +data+ and +meta+
     def write(temp_object, opts={})
-      File.transaction do
-        file = File.create!(metadata: temp_object.meta)
-        file.data = temp_object.data
+      File.new.tap do |file|
+        file.metadata = temp_object.meta
+        file.data     = temp_object.data
         file.save!
         return file.id.to_s
       end
     end
 
     def read(uid)
-      file = File.find(uid.to_i)
-      file.update_attributes!(accessed_at: Time.now)
+      file = File.where(id: uid.to_i).first
+      return nil if file.nil?
+
+      file.update_column(:accessed_at, Time.now)
       [ file.data, file.metadata ]
-    rescue ActiveRecord::RecordNotFound
-      nil
     end
 
     def destroy(uid)
